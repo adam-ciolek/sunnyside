25a7d6a56e1c04db4c4c9bc0e8965bf069ca4f17
Adam Ciołek <adasciolek@gmail.com>
2021-11-23 15:52:54 +0100

    add breakpoints

--- hamburger.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.querySelector('.navigation__hamburger svg')
-const nav = document.querySelector('.navigation__menu ul')
-
-
-hamburger.addEventListener('click', () => {
-    nav.classList.toggle('hamburger-toggle');
-    
-})
